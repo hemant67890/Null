27eaa62732555df1877f8d3ee7a0ad03bfbb3d0e
hemant67890 <godarasubhash337@gmail.com>
2025-01-04 13:28:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "My Bot"
-BOT_TOKEN = "7806457322:AAE42L9KgSuYrLqx1QFWf0HGaPQQMIZc5qU"
+BOT_TOKEN = ""
 API_ID = 28328736
 API_HASH = "802254a44896baa87f3083b7af36b2e5"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
