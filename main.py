cb439178eaa67012403de4effe2e1b070be2942b
hemant67890 <godarasubhash337@gmail.com>
2025-01-04 12:12:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ OWNER_IDS = [2052075731]  # Replace with the actual owner user IDs
 
 cookies_file_path = "modules/cookies.txt"
 # Global variables
-log_channel_id = -1002370315096
+log_channel_id = -1002360995423
 authorized_users = []
-ALLOWED_CHANNEL_IDS = [-1002370315096]
+ALLOWED_CHANNEL_IDS = []
 my_name = "𝐊𝐔𝐍𝐀𝐋❤️"
 overlay = None 
 accept_logs = 0
